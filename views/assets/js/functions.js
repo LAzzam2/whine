73fefaa1e5b7b6a7d7e0a05a87471861a3f57668
Laith <info@laithazzam.com>
2014-11-01 22:15:17 -0700

    disable scroll

--- views/assets/js/functions.js
+++ views/assets/js/functions.js
@@ -14,6 +14,70 @@
 		init: function(){
 			site.pullWhine();
 			site.clickFunc();
+			// site.scrollHandler();
+		},
+		scrollHandler: function(){
+			var that = this;
+			// left: 37, up: 38, right: 39, down: 40,
+			// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+			var keys = [37, 38, 39, 40, 32];
+
+			function preventDefault(e) {
+			  e = e || window.event;
+			  if (e.preventDefault)
+			      e.preventDefault();
+			  e.returnValue = false;  
+			}
+
+			function keydown(e) {
+			    for (var i = keys.length; i--;) {
+			        if (e.keyCode === keys[i]) {
+			            preventDefault(e);
+			            return;
+			        }
+			    }
+			}
+			function wheel(e) {
+			  $(document).on( 'DOMMouseScroll mousewheel', function(event) {
+			  	if($('body').hasClass('characterState')){
+				    if(event.deltaY == -1){
+
+				      name = $('#charactercarousel>.roundabout-in-focus').attr('data');
+				      name = name.replace(/\s/g, '');
+
+				      if(name == 'undefined'){
+
+				      }if(name != 'undefined'){
+				      	that.crystalState(name);
+				      }
+				    }
+			    };
+			  });
+			  $(document).on( 'DOMMouseScroll mousewheel', function(event) {
+			    if($('body').hasClass('crystalState')){
+				    if(event.deltaY == 1){
+				    	that.characterState();
+						that.closeCrystalSelection();
+				    }
+			    };
+			  });
+			  preventDefault(e);
+			}
+			window.disable_scroll = function() {
+			  if (window.addEventListener) {
+			      window.addEventListener('DOMMouseScroll', wheel, false);
+			  }
+			  window.onmousewheel = document.onmousewheel = wheel;
+			  document.onkeydown = keydown;
+			}
+
+			window.enable_scroll = function() {
+			    if (window.removeEventListener) {
+			        window.removeEventListener('DOMMouseScroll', wheel, false);
+			    }
+			    window.onmousewheel = document.onmousewheel = document.onkeydown = null;  
+			}
+			disable_scroll();
 		},
 		pullWhine: function(){
 		},
