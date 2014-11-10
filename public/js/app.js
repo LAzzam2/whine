6f4c196e07158cd5b4bded80290aa713b85d8f6d
Laith <info@laithazzam.com>
2014-11-09 22:16:27 -0800

    laith shit

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,14 @@
 angular.module( 'whine' , ['famous.angular'])
 .service('whineService', [function(Restangular) {
 }])
-.controller( 'HomeCtrl', ['$scope', function AppCtrl ( $scope ) {
-    $scope.test = "Angular works!";
-    $scope.stuff = [
-        'fdsa  ',
-        ' paosdpf ppaosd '
-    ]
+.controller('ScrollCtrl', ['$scope', '$famous', function($scope, $famous) {
+        
+    var EventHandler = $famous['famous/core/EventHandler'];
+    $scope.eventHandler = new EventHandler();
+    
+    $scope.options = {
+      scrollViewTwo: {
+      }
+    };
+
 }]);
