1d9baf7ca6d8d71c735607e684693eda7b40e873
Nathan Jordan <natedagreat27274@gmail.com>
2014-11-09 19:11:32 -0800

    famous angular working

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,10 @@
-angular.module( 'whine' , [])
+angular.module( 'whine' , ['famous.angular'])
 .service('whineService', [function(Restangular) {
 }])
 .controller( 'HomeCtrl', ['$scope', function AppCtrl ( $scope ) {
     $scope.test = "Angular works!";
+    $scope.stuff = [
+        'fdsa  ',
+        ' paosdpf ppaosd '
+    ]
 }]);
